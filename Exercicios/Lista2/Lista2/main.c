f28b10bb6fec85cebef26e2069bc0617b78544fa
Flávio Miranda de Farias <fmflavio@gmail.com>
2019-08-26 19:54:34 -0300

    Lista2

---
+++ Exercicios/Lista2/Lista2/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+
+int main(){
+    int i;
+    printf("Digite o valor do menu: \n");
+    printf("1 - Para execicio 1 \n");
+    printf("2 - Para execicio 2 \n");
+    printf("3 - Para execicio 3 \n");
+    printf("4 - Para execicio 4 \n");
+    printf("Outro valor para sair \n");
+    printf("---> ");
+    scanf("%d",&i);
+    switch(i){
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
