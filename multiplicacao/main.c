25f13bbe974db55b9f61ca98f933d1e7e2544767
Flávio Miranda de Farias <fmflavio@gmail.com>
2019-09-06 16:36:03 -0300

    Karatsuba

--- multiplicacao/main.c
+++ multiplicacao/main.c
@@ -1,12 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include <time.h>
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+/** Function to calculate length or number of digits in a number **/
+int getSize(long num){
+    int ctr = 0;
+    while (num != 0){
+        ctr++;
+        num /= 10;
+    }
+    return ctr;
+}
+/** Function to multiply two numbers **/
+unsigned long multiply(unsigned long x, unsigned long y, int base){
+    int size1 = getSize(x);
+    int size2 = getSize(y);
+    /** Maximum of lengths of number **/
+    int N = MAX(size1, size2);
+    /** for small values directly multiply **/
+    //if (x < base || y < base)
+    if (N < base)
+        return x * y;
+    /** max length divided, rounded up **/
+    N = (N / 2) + (N % 2);
+    /** multiplier **/
+    unsigned long m = (unsigned long)pow(base, N);
+    /** compute sub expressions **/
+    unsigned long b = x / m;
+    unsigned long a = x - (b * m);
+    unsigned long d = y / m;
+    unsigned long c = y - (d * N);
+    /** compute sub expressions **/
+    unsigned long z0 = multiply(a, c, base);
+    unsigned long z1 = multiply(a + b, c + d, base);
+    unsigned long z2 = multiply(b, d, base);
+
+    //return (z2*pow(base,2*(N/2))+((z1-z2-z0*pow(base,(N/2)))+z0));
+    return z0+((z1-z0-z2)*m)+(z2*(unsigned long)(pow(base,2*N)));
+}
 
 void * multVet(char *s1, char *s2, int t1, int t2,int base){
-    clock_t begin,end;//relacionado ao cronometro
-    double time_spent=0.0;//relacionado ao cronometro
     int i,j,cont=0,vaium=0,*sf=(int*)malloc(sizeof(int)*(t1*t2));
-    begin =clock();//inicio do cronometro
     for(i=0;i<t1;i++)
         for(j=0;j<t2;j++){
             sf[cont]=(s1[i]-48)*(s2[j]-48);
@@ -34,28 +71,21 @@ void * multVet(char *s1, char *s2, int t1, int t2,int base){
             else
                 vaium=div;
         }
-    end =clock();//fim do cronometro
     printf("\nString Final: ");
     if(vaium!=0)
         printf("%d ",vaium);
     for(i=0;i<cont;i++)
         printf("%d ",sf[i]);
-    //exibe cronometro
-    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-	printf("\n\nTime elpased is %lf seconds\n", time_spent);
 	free(sf);
 }
 
 void * somaVet(char *s1, char *s2, int t1, int t2,int base){
-    clock_t begin,end;//relacionado ao cronometro
-    double time_spent=0.0;//relacionado ao cronometro
     int i,vaium=0,*sf=(int*)malloc(sizeof(int)*t1);
-    begin =clock();//inicio do cronometro
     for(i=0;i<t1;i++)//insere o maior vetor no final
         sf[i]=(s1[i]-48);
     for(i=0;i<t1;i++)//soma do vetor menor com o vetor final
         sf[i+(t1-t2)]=sf[i+(t1-t2)]+(s2[i]-48);
-    printf("\nString combinada: ");
+    printf("\nString inter(char[]): ");
     for(i=0;i<t1;i++)
         printf("%d ",sf[i]);
     //converte o vetor bruto para o final
@@ -68,25 +98,23 @@ void * somaVet(char *s1, char *s2, int t1, int t2,int base){
             else
                 vaium=div;
         }
-    end =clock();//fim do cronometro
-    printf("\nString Final: ");
+    printf("\nString Final(char[]): ");
     if(vaium!=0)
         printf("%d ",vaium);
     for(i=0;i<t1;i++)
         printf("%d ",sf[i]);
-    //exibe cronometro
-    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-	printf("\n\nTime elpased is %lf seconds\n", time_spent);
 	free(sf);
 }
 
 int main(){
     while(1){
+        clock_t begin,end;//relacionado ao cronometro
+        double time_spent=0.0;//relacionado ao cronometro
         fflush(stdin);
         int tipo,base,t1,t2,i;
-        printf("\nDigite 1-Soma ou 2-Multiplicacao: ");
+        printf("\nDigite\n1-Soma(Proprio)\n2-Multiplicacao(Karatsuba):\n--> ");
         scanf(" %d",&tipo);
-        printf("Digite a Base: ");
+        printf("\nDigite a Base: ");
         scanf(" %d",&base);
         printf("Tamanho da string 1: ");
         scanf(" %d",&t1);
@@ -94,17 +122,18 @@ int main(){
         scanf(" %d",&t2);
         char *s1=(char*)malloc(sizeof(char)*t1),
             *s2=(char*)malloc(sizeof(char)*t2);
-        printf("\nString 1: ");
+        printf("\nString 1(char[]): ");
         for(i=0;i<t1;i++){
             s1[i]=(rand()%base)+48;
             printf("%c ",s1[i]);
         }
-        printf("\nString 2: ");
+        printf("\nString 2(char[]): ");
         for(i=0;i<t2;i++){
             s2[i]=(rand()%base)+48;
             printf("%c ",s2[i]);
         }
         printf("\n");
+        begin=clock();//inicio do cronometro
         if(tipo==1){
              if(t1>=t2)
                 somaVet(s1,s2,t1,t2,base);
@@ -112,16 +141,27 @@ int main(){
                 somaVet(s2,s1,t2,t1,base);
 
         } else {
+            long si1=0,si2=0;
+            si1=atol(s1);
+            si2=atol(s2);
+            printf("\nResultado: %lu",multiply(si1,si2,base));
+            /*
             if(t1>=t2)
                 multVet(s1,s2,t1,t2,base);
             else
                 multVet(s2,s1,t2,t1,base);
+            */
         }
+        end=clock();//fim do cronometro
+        //exibe cronometro
+        time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+        printf("\n\nTime elpased is %lf seconds\n", time_spent);
         free(s1);
         free(s2);
         printf("\nNovamente? (s/n): ");
         char c;
         scanf(" %c",&c);
+        system("cls");
         if(c!='s')
             return 0;
     }
