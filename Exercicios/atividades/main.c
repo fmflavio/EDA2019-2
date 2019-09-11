4d76557d0f0424bad1b6d1bc449d0dd881d2bafb
Flávio Miranda de Farias <fmflavio@gmail.com>
2019-09-11 18:00:55 -0300

    exercicios

--- Exercicios/atividades/main.c
+++ Exercicios/atividades/main.c
@@ -1,5 +1,75 @@
 #include <stdio.h>
 
+int **geraMat(int l,int c,int val1,int val2,int base){
+    int i,j;
+    //alocação dinamica
+    int **mat=(int**)malloc(sizeof(int*)*l);
+    for(i=0;i<l;i++)
+        mat[i]=(int*)malloc(sizeof(int)*c);
+    //gera matriz
+    for(i=0;i<l;i++)
+        for(j=0;j<c;j++){
+            if(i==0 && j==0)
+                mat[i][j]=(val1/base)*base;
+            else
+                if(i==0 && j==1)
+                    mat[i][j]=val1%base;
+                else
+                    if(i==1 && j==0)
+                        mat[i][j]=(val2/base)*base;
+                    else
+                        if(i==1 && j==1)
+                            mat[i][j]=val2%base;
+            //imprime a primeira matriz
+            printf("%d ",mat[i][j]);
+        }
+    printf("\n");
+    return mat;
+}
+void **multMat(int **mat,int l,int c){
+    int i,j,resp[l][c],total=0;
+    for(i=0;i<l;i++)
+        for(j=0;j<c;j++){
+            //multiplica os valores da matriz
+            // original geranto a seundária
+            resp[i][j]=mat[0][i]*mat[1][j];
+            printf("%d ",resp[i][j]);
+            //após exibir a matriz secundaria
+            //soma os valores em um inteiro
+            total+=resp[i][j];
+        }
+    printf("\n%d ",total);
+    return resp;
+}
+void multiplicacao(){
+    //multiplicação usando matrizes
+    int i,j,l=2,c=2,base=10,
+        val1=34,val2=13;//valores
+    int **mat=geraMat(l,c,val1,val2,base);
+    multMat(mat,l,c);
+}
+
+void selectionSort(int *vet,int n){
+    int i,j,menor,aux;
+    for(i=0;i<n-1;i++){
+        menor=i;
+        for(j=i+1;j<n;j++)
+            if(vet[menor]>vet[j])
+                menor=j;
+        if(menor!=i){
+            aux=vet[menor];
+            vet[menor]=vet[i];
+            vet[i]=aux;
+        }
+    }
+}
+void ordena(){
+    int i,n=10,vet[]={9,0,1,3,2,5,4,8,6,7};
+    selectionSort(vet,n);
+    for(i=0;i<n;i++)
+        printf("%d ",vet[i]);
+}
+
 int fibo3(int n){
     if(n<=1)
         return 1;
@@ -13,7 +83,6 @@ int fibo2(int n){
         c=a+b;
         a=b;
         b=c;
-        printf("%d ",c);
     }
     return c;
 }
@@ -23,8 +92,8 @@ void fibo(){
     printf("Digite o n fibonascci: ");
     scanf("%d",&n);
     if(n<=0)
-        exit(0);
-    printf("\n%d",fibo3(n));
+        return;
+    printf("\n%d",fibo2(n));
 }
 
 int fatorial2(int n){
@@ -38,7 +107,7 @@ void fatorial(){
     printf("Digite o n fatorial: ");
     scanf("%d",&n);
     if(n<=0)
-        exit(0);
+        return;
     printf("\n%d",fatorial2(n));
 }
 
@@ -76,25 +145,50 @@ void fibonascci(){
         ant=1,antant=1,cont=2;
     }
 }
-
+int mdc2(int x, int y){
+    if (y==0) //if(x%y==0)
+        return x; //return y
+    else
+        return mdc(y,x % y);
+}
 void mdc(){
-    int x,y,i,menor,maior,div;
+    int x,y,i,menor,div;
     while(1){
         printf("Digite dois numeros: ");
         scanf("%d %d",&x,&y);
         if(x<=0 && y<=0)
             break;
-        if(x>y){
-            maior=x;
+        if(x>y)
             menor=y;
-        } else {
-            maior=y;
+        else
             menor=x;
-        }
         for(i=1;i<menor;i++)
             if(x%i==0 && y%i==0)
                 div=i;
         printf("%d\n",div);
+    }
+}
+
+void sec_dec(){
+    int n,cont=0,contfinal=0,ant=0,i,x;
+    while(1){
+        printf("Digite o Tamanho: ");
+        scanf("%d",&n);
+        if(n<=0)
+            break;
+        for(i=0;i<n;i++){
+            printf("Digite valor %d: ",i+1);
+            scanf("%d",&x);
+            if(ant>x)
+                cont++;
+            else
+                cont = 1;
+            if(cont>contfinal)
+                contfinal=cont;
+            ant=x;
+        }
+        printf("Tamanho: %d\n",contfinal);
+        cont=contfinal=0;
     }
 }
 
@@ -115,27 +209,6 @@ void sec_cre(){
     }
     printf("Esta e a maior sequencia: %d\n",maior);
 }
-void sec_dec(){
-    int n,cont=0,contfinal=0,ant,i,x;
-    while(1){
-        printf("Digite o Tamanho: ");
-        scanf("%d",&n);
-        if(n<=0)
-            break;
-        for(i=0;i<n;i++){
-            scanf("%d",&x);
-            if(ant>x)
-                cont++;
-            else
-                cont = 1;
-            if(cont>contfinal)
-                contfinal=cont;
-            ant=x;
-        }
-        printf("Tamanho: %d\n",contfinal);
-        cont=contfinal=0;
-    }
-}
 
 int main(){
     int i;
@@ -147,6 +220,9 @@ int main(){
     printf("5 - Para execicio: \n");
     printf("6 - Para execicio: \n");
     printf("7 - Para execicio: \n");
+    printf("8 - Para execicio: \n");
+    printf("9 - Para execicio: \n");
+    printf("--> ");
     scanf("%d",&i);
     switch(i){
         case 1:
@@ -169,6 +245,12 @@ int main(){
             break;
         case 7:
             fibo();
+            break;
+        case 8:
+            ordena();
+            break;
+        case 9:
+            multiplicacao();
             break;
         default:
             break;
